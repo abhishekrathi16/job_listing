277b7127a5d261b5b4c369067bf6c351f3433e81
abhishekrathi16 <rathiabhishek53@gmail.com>
2024-04-13 23:57:53 +0530

    feat: added navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,44 @@
-import React from 'react'
+import React from "react";
+import { Menu } from "antd";
+import {
+  HomeOutlined,
+  SearchOutlined,
+  EyeOutlined,
+  SolutionOutlined,
+  PhoneOutlined,
+} from "@ant-design/icons";
+import "./Navbar.css";
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
-}
+    <div className="navbar">
+      {/* <div className="logo"> Company Logo</div> */}
+      <Menu mode="horizontal" theme="dark" className="custom-navbar">
+        <Menu.Item key="logo" className="logo">
+          Company Logo
+        </Menu.Item>
+        <Menu.Item key="home" icon={<HomeOutlined />}>
+          Home
+        </Menu.Item>
+        <Menu.Item key="find-jobs" icon={<SearchOutlined />}>
+          Find Jobs
+        </Menu.Item>
+        <Menu.Item key="browse" icon={<EyeOutlined />}>
+          Browse
+        </Menu.Item>
+        <Menu.Item key="success-stories" icon={<SolutionOutlined />}>
+          Success Stories
+        </Menu.Item>
+        <Menu.Item
+          key="contact-us"
+          icon={<PhoneOutlined />}
+          className="contact"
+        >
+          Contact Us
+        </Menu.Item>
+      </Menu>
+    </div>
+  );
+};
 
-export default Navbar
+export default Navbar;
